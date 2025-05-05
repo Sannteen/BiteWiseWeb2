8f9f64142d5f9e401a5f23edced5e04787538b2e
Ladybug90 <famousrunner@gmail.com>
2025-05-05 05:40:49 -0500

    update on profile

--- Models/ProfileViewModel.cs
+++ Models/ProfileViewModel.cs
@@ -2,13 +2,13 @@
 {
     public class ProfileViewModel
     {
-        public string? FullName { get; set; }
+        public string FullName { get; set; }
         public int? Age { get; set; }
         public double? Weight { get; set; }
         public double? Height { get; set; }
-        public string? Gender { get; set; }
-        public string? FitnessGoal { get; set; }
-        public string? ActivityLevel { get; set; }
+        public string Gender { get; set; }
+        public string FitnessGoal { get; set; }
+        public string ActivityLevel { get; set; }
         public string? ProfilePicturePath { get; set; }
     }
 }
